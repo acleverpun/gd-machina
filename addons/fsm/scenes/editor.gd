77f1b86b1a9b36ce78d2eff3f6c9bd5a1f432932
Ross Hadden <rosshadden@gmail.com>
2022-02-13 14:04:39 -0500

    Added mappings and context menu.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- addons/fsm/scenes/editor.gd
+++ addons/fsm/scenes/editor.gd
@@ -1,1 +1,11 @@
 extends Control
+
+@onready var context := $context as PopupMenu
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_RIGHT:
+				if event.pressed:
+					context.popup()
+					context.position = get_viewport().get_mouse_position()
