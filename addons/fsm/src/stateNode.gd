598c62883e6d7f400dc7f9ce69fb864a04016afa
Ross Hadden <rosshadden@gmail.com>
2022-02-17 03:04:31 -0500

    Let node titles get edited.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- addons/fsm/src/stateNode.gd
+++ addons/fsm/src/stateNode.gd
@@ -1,4 +1,15 @@
 class_name StateNode extends GraphNode
 
+@onready var container = $container as HBoxContainer
+@onready var text = container.get_node("text") as LineEdit
+@onready var edit = container.get_node("edit") as Button
+
 func _ready() -> void:
 	pass
+
+func _on_edit_toggled(editMode: bool) -> void:
+	text.visible = editMode
+	text.editable = editMode
+
+	if not editMode:
+		title = text.text
