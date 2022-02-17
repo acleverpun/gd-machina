9092a0e2798ab05ef1269fe8714b135f5b6d072c
Ross Hadden <rosshadden@gmail.com>
2022-02-17 16:05:49 -0500

    Only show fsm control in bottom panel if fsm node selected.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- addons/fsm/plugin.gd
+++ addons/fsm/plugin.gd
@@ -4,11 +4,27 @@ extends EditorPlugin
 const FsmEditor := preload("./scenes/editor.tscn")
 
 var editor: Control
+var selection: EditorSelection
 
 func _enter_tree() -> void:
+	selection = get_editor_interface().get_selection()
+	selection.connect("selection_changed", _on_selection_changed)
+
 	editor = FsmEditor.instantiate()
-	add_control_to_bottom_panel(editor, "FSM")
 
 func _exit_tree() -> void:
 	remove_control_from_bottom_panel(editor)
 	editor.queue_free()
+
+func toggle(visible: bool = true) -> void:
+	if visible:
+		add_control_to_bottom_panel(editor, "FSM")
+	else:
+		remove_control_from_bottom_panel(editor)
+
+func _on_selection_changed() -> void:
+	for node in selection.get_selected_nodes():
+		if node is Fsm:
+			toggle(true)
+			return
+	toggle(false)
