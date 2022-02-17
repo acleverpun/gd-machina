198ad91f0c7758ba056eb4759745a004b4b497db
Ross Hadden <rosshadden@gmail.com>
2022-02-17 15:00:09 -0500

    Better edit UX.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- addons/fsm/src/stateNode.gd
+++ addons/fsm/src/stateNode.gd
@@ -2,14 +2,26 @@ class_name StateNode extends GraphNode
 
 @onready var container = $container as HBoxContainer
 @onready var text = container.get_node("text") as LineEdit
-@onready var edit = container.get_node("edit") as Button
+@onready var editBtn = container.get_node("edit") as Button
 
 func _ready() -> void:
 	pass
 
-func _on_edit_toggled(editMode: bool) -> void:
-	text.visible = editMode
-	text.editable = editMode
+func save() -> void:
+	title = text.text
 
-	if not editMode:
-		title = text.text
+func edit(active: bool) -> void:
+	text.visible = active
+	text.editable = active
+
+	if active:
+		text.grab_focus()
+	else:
+		editBtn.set_pressed_no_signal(false)
+		save()
+
+func _on_edit_toggled(active: bool) -> void:
+	edit(active)
+
+func _on_text_text_submitted(value: String) -> void:
+	edit(false)
